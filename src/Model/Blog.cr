fe6a128e578ce87ff51de9f57b4a33797adcd554
Drago <assabyn@gmail.com>
2021-12-10 22:57:19 +0600

    add the blog model and create 2 methods

---
+++ src/Model/Blog.cr
@@ -0,0 +1,46 @@
+require "totem"
+
+struct Post
+  property id : Int32 = 0
+  property title : String = "undefind"
+  property miniInfo : String = "undefind"
+  property body : String = "undefind"
+end
+
+class BlogModel
+  def get_post_list
+  end
+
+  def create_post(post : Post)
+    totem = Totem.new
+    totem.set_defaults({
+      "id"           => post.id,
+      "postTitle"    => post.title,
+      "postMiniInfo" => post.miniInfo,
+      "postBody"     => post.body,
+    })
+
+    begin
+      totem.store_file!("./config/#{post.id}.yaml", force: true)
+    rescue e
+      puts "Fatal error config file: #{e.message}"
+    end
+  end
+
+  def get_post_by_id(id : Int32)
+    totem = Totem.from_file "./config/#{id}.yaml"
+    post = Post.new
+    post.id = id
+    post.title = totem.get("postTitle").as_s
+    post.miniInfo = totem.get("postMiniInfo").as_s
+    post.body = totem.get("postBody").as_s
+    return post
+  end
+
+  def update_post_by_id(newParams : Post)
+  end
+
+  def delete_post_by_id(id : Int32)
+  end
+end
+
